94fdf7d650ad276cdde567000090da50c9819ccf
karthikgcan <karthikganeshan.rmd@gmail.com>
2021-05-10 22:44:49 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          This is karthik app
         </p>
         <a
           className="App-link"
