ac5f6f98a2f33816222ad72ca50efdaf7c092677
karthikgcan <karthikganeshan.rmd@gmail.com>
2021-05-14 11:15:19 +0530

    new

---
+++ src/containers/login.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import ReactDOM from 'react-dom';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./login.css";
+import axios from 'axios';
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    axios.post('http://localhost:5000/posts', {
+      username: email,
+      password: password
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  }
+
+  return (
+      
+    <div className="container">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control className="field"
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control className="field"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from "./containers/login";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const myfirstelement = <h1>Hello React!</h1>
+
+ReactDOM.render(<Login/>, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
